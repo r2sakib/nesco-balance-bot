2d8499ae4ff8c640020d6d94be6f5033cf179b45
Sadman Sakib <sakibsadman035@gmail.com>
2022-06-24 17:47:49 +0600

    Added error handling

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from inspect import trace
+from sched import scheduler
 import telebot
 import os
 from threading import Thread
 from dotenv import load_dotenv
 from datetime import datetime
 from time import sleep
+import traceback
+
+import urllib3
 
 import data_collector 
 
@@ -24,6 +29,12 @@ bot = telebot.TeleBot(API_KEY, parse_mode='HTML')
 
 command_name = None
 starting_message = "/balance - Check current balance\n/balance_default - Check current balance with preset customer no.\n/last_recharge - Check last recharge details\n/last_recharge_default - Check last recharge details with preset customer no.\n/notify - Get balance update if balance is less than ৳100\n/notify_daily - Get balance update daily at 06:00 AM"
+
+
+def logger(log):
+    with open('logs.md', 'a') as log_file:
+        timestamp = datetime.now()
+        log_file.write(f'\n\n{timestamp}\n\n```{log}```\n\n---')
 
 
 def message_formatter(type: str, cust_no) -> str:
@@ -141,190 +152,239 @@ def set_cust_no(doc_id: str, field: str, cust_no: str):
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    global command_name
-    command_name = 'start'
-    bot.send_message(message.chat.id, starting_message)
+    try:
+        global command_name
+        command_name = 'start'
+        bot.send_message(message.chat.id, starting_message)
 
+    except Exception:
+        logger(traceback.format_exc())
 
 @bot.message_handler(commands=['balance'])
 def balance(message):
-    global command_name
-    command_name = 'balance'
-    bot.send_message(message.chat.id, 'Enter a customer number.')
+    try:
+        global command_name
+        command_name = 'balance'
+        bot.send_message(message.chat.id, 'Enter a customer number.')
+
+    except Exception:
+        logger(traceback.format_exc())
 
 
 @bot.message_handler(commands=['last_recharge'])
 def recharge(message):
-    global command_name
-    command_name = 'recharge'
-    bot.send_message(message.chat.id, 'Enter a customer number.')
+    try:
+        global command_name
+        command_name = 'recharge'
+        bot.send_message(message.chat.id, 'Enter a customer number.')
 
+    except Exception:
+        logger(traceback.format_exc())
 
 @bot.message_handler(commands=['notify'])
 def notify(message):
-    global command_name
-    command_name = 'notify'
-    bot.send_message(message.chat.id, 'Enter a customer number.')
+    try:
+        global command_name
+        command_name = 'notify'
+        bot.send_message(message.chat.id, 'Enter a customer number.')
 
+    except Exception:
+        logger(traceback.format_exc())
 
 @bot.message_handler(commands=['notify_daily'])
 def notify_daily(message):
-    global command_name
-    command_name = 'notify_daily'
-    bot.send_message(message.chat.id, 'Enter a customer number.')
+    try:
+        global command_name
+        command_name = 'notify_daily'
+        bot.send_message(message.chat.id, 'Enter a customer number.')
+
+    except Exception:
+        logger(traceback.format_exc())
 
 
 @bot.message_handler(commands=['last_recharge_default'])
 def last_rechrage_default(message):
-    global command_name
-    command_name = 'last_recharge_default'
+    try:
+        global command_name
+        command_name = 'last_recharge_default'
 
-    tg_id = str(message.chat.id)
-    cust_nos = get_cust_nos(tg_id, 'presets')
+        tg_id = str(message.chat.id)
+        cust_nos = get_cust_nos(tg_id, 'presets')
 
-    if cust_nos == None:
-        bot.send_message(tg_id, 'Enter a customer number.  (This step is only for initialization)')
-    
-    else:
-        cust_no = cust_nos[0]
-        msg = bot.send_message(tg_id, 'Getting data...')
-        response = message_formatter('recharge', cust_no)
-
-        if response != False:
-            bot.edit_message_text(response, tg_id, msg.message_id)
+        if cust_nos == None:
+            bot.send_message(tg_id, 'Enter a customer number.  (This step is only for initialization)')
+        
         else:
-            bot.edit_message_text('Please try again.', tg_id, msg.message_id)
+            cust_no = cust_nos[0]
+            msg = bot.send_message(tg_id, 'Getting data...')
+            response = message_formatter('recharge', cust_no)
+
+            if response != False:
+                bot.edit_message_text(response, tg_id, msg.message_id)
+            else:
+                bot.edit_message_text('Please try again.', tg_id, msg.message_id)
+
+    except Exception:
+        logger(traceback.format_exc())
+        try:
+            bot.send_message('Error occured. Try again after some time.', tg_id)
+        except Exception:
+            logger(traceback.format_exc())
+
 
 
 @bot.message_handler(commands=['balance_default'])
 def balance_default(message):
-    global command_name
-    command_name = 'balance_default'
+    try:
+        global command_name
+        command_name = 'balance_default'
 
-    tg_id = str(message.chat.id)
-    cust_nos = get_cust_nos(tg_id, 'presets')
+        tg_id = str(message.chat.id)
+        cust_nos = get_cust_nos(tg_id, 'presets')
 
-    if cust_nos == None:
-        bot.send_message(tg_id, 'Enter a customer number.  (This step is only for initialization)')
-    
-    else:
-        cust_no = cust_nos[0]
-        msg = bot.send_message(tg_id, 'Getting data...')
-        response = message_formatter('balance', cust_no)
-
-        if response != False:
-            bot.edit_message_text(response, tg_id, msg.message_id)
+        if cust_nos == None:
+            bot.send_message(tg_id, 'Enter a customer number.  (This step is only for initialization)')
+        
         else:
-            bot.edit_message_text('Please try again.', tg_id, msg.message_id)
+            cust_no = cust_nos[0]
+            msg = bot.send_message(tg_id, 'Getting data...')
+            response = message_formatter('balance', cust_no)
+
+            if response != False:
+                bot.edit_message_text(response, tg_id, msg.message_id)
+            else:
+                bot.edit_message_text('Please try again.', tg_id, msg.message_id)
+
+    except Exception:
+        logger(traceback.format_exc())
+        try:
+            bot.send_message('Error occured. Try again after some time.', tg_id)
+        except Exception:
+            logger(traceback.format_exc())
 
 
 @bot.message_handler()
 def echo(message):
-    if command_name == 'start':
-      bot.send_message(message.chat.id, starting_message)
-    
-    if command_name == 'balance' or command_name == 'recharge':
+    try:
+        if command_name == 'start':
+            bot.send_message(message.chat.id, starting_message)
+        
+        if command_name == 'balance' or command_name == 'recharge':
 
-        msg = bot.send_message(message.chat.id, 'Getting data...')
-        response = message_formatter(command_name, message.text)
+            msg = bot.send_message(message.chat.id, 'Getting data...')
+            response = message_formatter(command_name, message.text)
 
-        if response != False:
-            bot.edit_message_text(response, message.chat.id, msg.message_id)
-        else:
-            bot.edit_message_text('Enter a valid customer number, or try again.', message.chat.id, msg.message_id)
-
-
-    if command_name == 'notify':
-
-        tg_id = str(message.chat.id)
-
-        if not doc_exists(tg_id):
-            create_doc(message.chat)
-
-        if doc_exists(tg_id):
-            set_cust_no(tg_id, 'notify', message.text)
-
-        bot.send_message(message.chat.id, 'Setup success! You will get a message if remaining balance balance is less than ৳100.')
-    
-
-    if command_name == 'notify_daily':
-
-        tg_id = str(message.chat.id)
-
-        if not doc_exists(tg_id):
-            create_doc(message.chat)
-
-        if doc_exists(tg_id):
-            set_cust_no(tg_id, 'notify_daily', message.text)
-
-        bot.send_message(message.chat.id, 'Setup success! You will get a balance update eveyday at 06:00 AM.')
+            if response != False:
+                bot.edit_message_text(response, message.chat.id, msg.message_id)
+            else:
+                bot.edit_message_text('Enter a valid customer number, or try again.', message.chat.id, msg.message_id)
 
 
-    if command_name == 'last_recharge_default':
+        if command_name == 'notify':
 
-        tg_id = str(message.chat.id)
+            tg_id = str(message.chat.id)
 
-        if not doc_exists(tg_id):
-            create_doc(message.chat)
+            if not doc_exists(tg_id):
+                create_doc(message.chat)
 
-        if doc_exists(tg_id):
-            set_cust_no(tg_id, 'presets', message.text)
+            if doc_exists(tg_id):
+                set_cust_no(tg_id, 'notify', message.text)
 
-        bot.send_message(message.chat.id, "Setup success! Just send the command /last_recharge_default next time to see last recharge history without entering customer no.")
+            bot.send_message(message.chat.id, 'Setup success! You will get a message if remaining balance balance is less than ৳100.')
+        
+
+        if command_name == 'notify_daily':
+
+            tg_id = str(message.chat.id)
+
+            if not doc_exists(tg_id):
+                create_doc(message.chat)
+
+            if doc_exists(tg_id):
+                set_cust_no(tg_id, 'notify_daily', message.text)
+
+            bot.send_message(message.chat.id, 'Setup success! You will get a balance update eveyday at 06:00 AM.')
 
 
-    if command_name == 'balance_default':
+        if command_name == 'last_recharge_default':
 
-        tg_id = str(message.chat.id)
+            tg_id = str(message.chat.id)
 
-        if not doc_exists(tg_id):
-            create_doc(message.chat)
+            if not doc_exists(tg_id):
+                create_doc(message.chat)
 
-        if doc_exists(tg_id):
-            set_cust_no(tg_id, 'presets', message.text)
+            if doc_exists(tg_id):
+                set_cust_no(tg_id, 'presets', message.text)
 
-        bot.send_message(message.chat.id, 'Just send the command /balance_default next time to see balance without entering customer no.')
+            bot.send_message(message.chat.id, "Setup success! Just send the command /last_recharge_default next time to see last recharge history without entering customer no.")
+
+
+        if command_name == 'balance_default':
+
+            tg_id = str(message.chat.id)
+
+            if not doc_exists(tg_id):
+                create_doc(message.chat)
+
+            if doc_exists(tg_id):
+                set_cust_no(tg_id, 'presets', message.text)
+
+            bot.send_message(message.chat.id, 'Just send the command /balance_default next time to see balance without entering customer no.')
+
+    except Exception:
+        logger(traceback.format_exc())
+        try:
+            bot.send_message('Error occured. Try again after some time.', tg_id)
+        except Exception:
+            logger(traceback.format_exc())
 
 
 def notifier():
 
-    sent_notify_to: list[str] = []
+    try:
+        sent_notify_to: list[str] = []
 
-    docs_notify = users.where('notify', u'!=', 'null').stream()
-    for doc in docs_notify:
-        cust_no = doc.to_dict()['notify'][0]
-        tg_id = doc.to_dict()['tg_id']
+        docs_notify = users.where('notify', u'!=', 'null').stream()
+        for doc in docs_notify:
+            cust_no = doc.to_dict()['notify'][0]
+            tg_id = doc.to_dict()['tg_id']
 
-        balance = data_collector.get_balance(cust_no=cust_no)
+            balance = data_collector.get_balance(cust_no=cust_no)
 
-        if float(balance) < 100: 
-            response = message_formatter('low_balance', cust_no)
-            bot.send_message(tg_id, response)
-        
-        sent_notify_to.append((cust_no, tg_id))
+            if float(balance) < 100: 
+                response = message_formatter('low_balance', cust_no)
+                bot.send_message(tg_id, response)
+            
+            sent_notify_to.append((cust_no, tg_id))
 
 
-    docs_notify_daily = users.where('notify_daily', '!=', 'null').stream()
-    for doc in docs_notify_daily:
-        cust_no = doc.to_dict()['notify_daily'][0]
-        tg_id = doc.to_dict()['tg_id']
+        docs_notify_daily = users.where('notify_daily', '!=', 'null').stream()
+        for doc in docs_notify_daily:
+            cust_no = doc.to_dict()['notify_daily'][0]
+            tg_id = doc.to_dict()['tg_id']
 
-        if (cust_no, tg_id) not in sent_notify_to:
-            response = message_formatter('balance', cust_no)
-            bot.send_message(tg_id, response)
+            if (cust_no, tg_id) not in sent_notify_to:
+                response = message_formatter('balance', cust_no)
+                bot.send_message(tg_id, response)
+    
+    except Exception:
+        logger(traceback.format_exc())
 
 
 def notifier_time():
     while True:
-        if datetime.now().hour == 00 and datetime.now().minute > 00:
+        if datetime.now().hour == 17 and datetime.now().minute > 00:
             notifier()
             sleep(3600)
         else:
             sleep(1800)
 
+try:
+    bot_thread = Thread(target=bot.polling)
+    bot_thread.start()
 
-bot_thread = Thread(target=bot.polling)
-bot_thread.start()
+    notifier_thread = Thread(target=notifier_time)
+    notifier_thread.start()
 
-notifier_thread = Thread(target=notifier_time)
-notifier_thread.start()
+except Exception:
+    logger(traceback.format_exc())
